3809acfdc7c36583e258e584c33ce55853f9de24
chenjing <chenjing@autohome.com.cn>
2018-08-24 13:48:54 +0800

    Initial commit

---
+++ src/main/java/com/cathy/cathyblog/CathyblogApplication.java
@@ -0,0 +1,12 @@
+package com.cathy.cathyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CathyblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CathyblogApplication.class, args);
+    }
+}
---
+++ src/test/java/com/cathy/cathyblog/CathyblogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cathy.cathyblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CathyblogApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
